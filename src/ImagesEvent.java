206a1474bb16435df21f92b8724020a6a1fa6037
rawalokes <rawallokesh000@gmail.com>
2022-01-20 21:39:09 +0545

    calculator

---
+++ src/ImagesEvent.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ImagesEvent extends JFrame{
+	
+	JLabel jLabel;
+	public void resizeImageStr(String url) {
+		
+	}
+	
+	
+	
+	
+	public ImagesEvent() {
+		setTitle("DEMO");
+		setBounds(0, 0,400, 400);
+		
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ImagesEvent event= new ImagesEvent();
+	}
+
+}
